50418b481c7b3fe41bfd72b1abfee0df59296531
boikoro <roman.boiko@ubs.com>
2013-03-23 13:42:35 +0000

    bot_trust practise - solved

--- practise/bot_trust_2011_qualification.clj
+++ practise/bot_trust_2011_qualification.clj
@@ -1,3 +1,53 @@
+(declare make-step)
+
+(defn solve-problem [input]
+	(loop [stepsToDo (map #(clojure.string/split % #"\s") (re-seq #"\w\s\d+" input))
+		   positions {\O 1 \B 1 \T 0}]
+		(if (.isEmpty stepsToDo)
+			(positions \T)
+		(recur (rest stepsToDo) (make-step stepsToDo positions)))))
+
+(defn positionFromStep [step]
+	(Integer/parseInt (nth step 1)))
+
+(defn opponent [toBot]
+	(if (= toBot \B) \O \B))
+
+(defn targetOpponentPosition [bot botCurrentPosition stepsToGo]
+	(if (.isEmpty stepsToGo)
+		botCurrentPosition
+		(if (= (first (first (first stepsToGo))) bot)
+			(positionFromStep (first stepsToGo))
+			(targetOpponentPosition bot botCurrentPosition (rest stepsToGo)))))
+
+(defn difff [pos1 pos2]
+	(Math/abs (- pos1 pos2)))
+
+(defn nextPositionAfterDiff [currentPos, targetPos, diff]
+	(let [diff12 (difff currentPos targetPos)]
+	(if (or (= diff12 0) (< diff12 diff))
+		targetPos
+		(if (< currentPos targetPos)
+			(+ currentPos diff)
+			(- currentPos diff)))))
+
+(defn make-step [steps, positions]
+	(let [target (first steps)
+		  currentBot (first (first target))
+		  currentPosition (positions currentBot)
+		  targetPosition (positionFromStep target)
+		  diff (+ 1 (difff targetPosition currentPosition))
+
+		  opponentBot (opponent currentBot)
+		  opponentCurrentPosition (positions opponentBot)
+		  opponentTargetPosition (targetOpponentPosition opponentBot opponentCurrentPosition (rest steps))
+		  opponentNextPosition (nextPositionAfterDiff opponentCurrentPosition, opponentTargetPosition, diff)
+		  ]
+		(conj positions {\T (+ diff (positions \T)) currentBot targetPosition opponentBot opponentNextPosition})))
+
+; ========================
+; ====INFRASTRUCTURE
+; ========================
 (defn read-lines [filename]
 	(line-seq (clojure.java.io/reader (clojure.java.io/file filename))))
 
@@ -13,11 +63,13 @@
 
 (def input (read-lines input-file))
 
-(defn number-of-steps [task]
-	(Integer/parseInt (subs task 0 1)))
 
-(with-open [w (clojure.java.io/writer output-file :append false)]
-	(doseq [i (range 1 (+ 1 (Integer/parseInt (first input)))) ]
-		(let [output-line (format "Case#%d: %d\n" i (number-of-steps (nth input i)))]
-			(print output-line)
-			(.write w output-line))))
+(defn solve-problem-suite []
+	(with-open [w (clojure.java.io/writer output-file :append false)]
+		(doseq [i (range 1 (+ 1 (Integer/parseInt (first input)))) ]
+			(let [output-line (format "Case #%d: %d\n" i (solve-problem (nth input i)))]
+				(print output-line)
+				(.write w output-line)))))
+
+(solve-problem-suite)
+
